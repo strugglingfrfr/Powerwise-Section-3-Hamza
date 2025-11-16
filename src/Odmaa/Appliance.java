7f718226a448638c3cac5898c2e016af9e5ecdf3
Odmaa Davaa <odmaa1008@gmail.com>
2025-11-16 19:13:32 +0000

    update

--- src/Odmaa/Appliance.java
+++ src/Odmaa/Appliance.java
@@ -9,5 +9,59 @@ package Odmaa;
  * @author apple
  */
 public class Appliance {
+    private String applianceName;
+    private double energyUsage;
+    private double wattage;
+    private double hours;
+
+    public Appliance() {
+    }
+
+    public Appliance(String applianceName, double energyUsage, double wattage, double hours) {
+        this.applianceName = applianceName;
+        this.energyUsage = energyUsage;
+        this.wattage = wattage;
+        this.hours = hours;
+    }
+
+    public String getApplianceName() {
+        return applianceName;
+    }
+
+    public void setApplianceName(String applianceName) {
+        this.applianceName = applianceName;
+    }
+
+    public double getEnergyUsage() {
+        return energyUsage;
+    }
+
+    public void setEnergyUsage(double energyUsage) {
+        this.energyUsage = energyUsage;
+    }
+
+    public double getWattage() {
+        return wattage;
+    }
+
+    public void setWattage(double wattage) {
+        this.wattage = wattage;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
     
+    public double claculateEnergyUsage(){
+        return energyUsage = wattage * hours ;
+    }
+    
+    @Override
+    public String toString(){
+        return applianceName + " : energy usage is " + energyUsage;
+    }
 }
